8d8a492634244549080082d1a271654aa719a77a
Бурлака Артур Евгеньевич <burlakaae@sibur.ru>
2020-04-09 00:10:15 +0300

    task1-3 refactor

--- DML/task1-3.sql
+++ DML/task1-3.sql
@@ -2,17 +2,24 @@ WITH gte_1k AS (
     SELECT us.uid,
            us.country
     FROM "default".tb_users us
-    JOIN "default".tb_logins log ON us.uid = log.user_uid
-    JOIN billing.tb_operations op ON log.login = op.login
+             JOIN "default".tb_logins log ON us.uid = log.user_uid
+             JOIN billing.tb_operations op ON log.login = op.login
     WHERE operation_type = 'deposit'
     GROUP BY us.uid, us.country
     HAVING AVG(op.amount) >= 1000
+-- выбор клиентов с средним депозитом >= 1000
+),
+all_clients AS (
+    SELECT
+        count(uid) as count_all,
+        country
+    FROM "default".tb_users
+    GROUP BY country
+-- подсчет всех клиентов по странам
 )
-SELECT
-    us.country,
-    count(distinct us.uid) as count_all,
-    count(distinct gte_1k.uid) as avg_dep_gte_1k
-FROM "default".tb_users us
-JOIN gte_1k ON us.country = gte_1k.country
-GROUP BY us.country
-ORDER BY us.country;
+SELECT gte_1k.country, count(gte_1k.uid) as avg_dep_gte_1k, count_all
+FROM gte_1k
+join all_clients on all_clients.country = gte_1k.country
+GROUP BY gte_1k.country, count_all
+ORDER BY country;
+
